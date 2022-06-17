84ea4ab18c3549c8f55ef2c46e4324da823d48bb
liamswenson <liamswenson@riseup.net>
2022-06-17 16:26:23 -0500

    Fixed starting value in range

--- solutions/6-sum-square-difference.rs
+++ solutions/6-sum-square-difference.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut sum_of_squares = 0;
     let mut square_of_sum = 0;
 
-    for i in ..=100 {
+    for i in 0..=100 {
         sum_of_squares += i * i;
         square_of_sum += i;
     }
